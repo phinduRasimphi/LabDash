d46e76f802c3287cb49b9e307399fd22ebd6fe9c
phindulo rasimphi <phindu.ras2003@gmail.com>
2026-06-23 14:31:56 +0200

    Add project files.

---
+++ LabDash/Areas/Identity/Data/LabUser.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabDash.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the LabUser class
+public class LabUser : IdentityUser
+{
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Text)]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Text)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [NotMapped]
+    public string FullName => $"{FirstName} {LastName}";
+
+
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Text)]
+    public string Gender { get; set; }
+
+
+
+    [Required(ErrorMessage = "Phone Number is required")]
+    [Display(Name = "Phone Number")]
+    [DataType(DataType.PhoneNumber)]
+    [RegularExpression(@"^1?[0-9]{10}$", ErrorMessage = "Not a valid Phone number")]
+    public string PhoneNumb { get; set; }
+
+
+
+    [Required]
+    [Display(Name = "Creation Date of Account")]
+    [DataType(DataType.DateTime)]
+    public DateTime Timestamp_AccountCreated { get; set; } = DateTime.Now;
+
+}
+
---
+++ LabDash/Program.cs
@@ -0,0 +1,121 @@
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LabDash.Areas.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<LabDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+builder.Services.AddIdentity<LabUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LabDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+using (var scope = app.Services.CreateScope())
+{
+
+
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<LabDbContext>();
+
+    // Initialize RoleManager
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();// look in to
+
+    // Check if "Admin" role exists, if not, create it
+    if (!roleManager.RoleExistsAsync("Admin").Result)
+    {
+        var role = new IdentityRole("Admin");
+        roleManager.CreateAsync(role).Wait();
+    }
+
+    // Check if "Nurse" role exists, if not, create it
+    if (!roleManager.RoleExistsAsync("Patient").Result)
+    {
+        var role = new IdentityRole("Patient");
+        roleManager.CreateAsync(role).Wait();
+    }
+
+    // Check if "Surgeon" role exists, if not, create it
+    if (!roleManager.RoleExistsAsync("Lab_Manager").Result)
+    {
+        var role = new IdentityRole("Lab_Manager");
+        roleManager.CreateAsync(role).Wait();
+    }
+
+    // Check if "Pharmacist" role exists, if not, create it
+    if (!roleManager.RoleExistsAsync("Doctor").Result)
+    {
+        var role = new IdentityRole("Doctor");
+        roleManager.CreateAsync(role).Wait();
+    }
+
+    // Check if "Anaesthesiologist" role exists, if not, create it
+    if (!roleManager.RoleExistsAsync("Lab_Technician").Result)
+    {
+        var role = new IdentityRole("Lab_Technician");
+        roleManager.CreateAsync(role).Wait();
+    }
+
+
+
+
+    //using (var scope = app.Services.CreateScope())
+    //{
+    //    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<DayUser>>();
+
+    //    string email = "admin@gmail.com";
+    //    string password = "Password123!";
+
+    //    if (await userManager.FindByEmailAsync(email) == null)
+    //    {
+    //        var user = new DayUser();
+    //        user.Email = email;
+    //        user.UserName = email;
+    //        user.EmailConfirmed = true;
+    //        user.FirstName = "DANIEL";
+    //        user.LastName = "GASTAVO";
+    //        user.HCRN = 0;
+    //        user.PhoneNumb = "0741234567";
+    //        user.Gender = "Female";
+
+    //        user.Timestamp_AccountCreated = DateTime.Now;
+    //        await userManager.CreateAsync(user, password);
+
+    //        await userManager.AddToRoleAsync(user, "Admin");
+    //    }
+    //}
+
+    app.Run();
+}
