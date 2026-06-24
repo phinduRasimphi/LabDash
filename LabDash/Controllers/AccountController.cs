273011141ab3f99028827a573dc7cdeeab6db914
liyema-s <liyemasipika@gmail.com>
2026-06-24 15:14:17 +0200

    Login/Layout

---
+++ LabDash/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using LabDash.Areas.Identity.Data;
+using LabDash.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabDash.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly LabDbContext _context;
+        private readonly UserManager<LabUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUserStore<LabUser> _userStore;
+        private readonly SignInManager<LabUser> _signInManager;
+
+        public AccountController(LabDbContext dbContext,
+            UserManager<LabUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IUserStore<LabUser> userStore,
+
+            SignInManager<LabUser> signInManager)
+
+        {
+            _userManager = userManager;
+            _context = dbContext;
+            _roleManager = roleManager;
+            _userStore = userStore;
+
+            _signInManager = signInManager;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                 if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError("", "Email not confirmed.");
+                        return View(model);
+                    }
+
+                    var result = await _signInManager.PasswordSignInAsync(
+                        model.Email,
+                        model.Password,
+                        model.RememberMe,
+                        lockoutOnFailure: true);
+
+                    if (result.Succeeded)
+                    {
+                        TempData["SuccessMessage"] = "Welcome to your work environment!";
+
+                        if (await _userManager.IsInRoleAsync(user, "Admin"))
+                            return RedirectToAction("Dashboard", "Admin");
+
+                        if (await _userManager.IsInRoleAsync(user, "Doctor"))
+                            return RedirectToAction("Dashboard", "Doctor");
+
+                        if (await _userManager.IsInRoleAsync(user, "Lab_Technician"))
+                            return RedirectToAction("Dashboard", "LabTechnician");
+
+                        if (await _userManager.IsInRoleAsync(user, "Lab_Manager"))
+                            return RedirectToAction("Dashboard", "LabManager");
+
+                        if (await _userManager.IsInRoleAsync(user, "Patient"))
+                            return RedirectToAction("Dashboard", "Patient");
+
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    if (result.IsLockedOut)
+                    {
+                        ModelState.AddModelError("", "Account is locked.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid password.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User not found.");
+                }
+            }
+
+            return View(model);
+}
+
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ LabDash/Models/RegisterModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabDash.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^1?[0-9]{10}$", ErrorMessage = "Not a valid Phone number.")]
+        public string PhoneNumb { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = "Password123!";
+
+
+
+        [Required(ErrorMessage = "Creation Date of Account is required.")]
+        [Display(Name = "Creation Date of Account")]
+        [DataType(DataType.DateTime)]
+        public DateTime Timestamp_AccountCreated { get; set; } = DateTime.Now;
+    }
+}
